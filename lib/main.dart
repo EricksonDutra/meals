a1907b0bde87fd12391499f08c10feacac0cebb7
EricksonDutra <ericksond10@gmail.com>
2021-09-22 07:09:31 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        title: Text('DeliMeals'),
+      ),
+      // ignore: prefer_const_constructors
+      body: Center(
+        // ignore: prefer_const_constructors
+        child: Text('Navegar é preciso!!'),
+      ),
+    );
+  }
+}
